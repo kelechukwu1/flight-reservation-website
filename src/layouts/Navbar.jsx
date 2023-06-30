44bcb3b7c3f2d1fbd998214c7f8fec3493545549
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-06-30 04:12:39 +0100

    made changes to SelectBound.jsx

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -8,105 +8,105 @@ import BookFlightForm from "../components/BookFlightForm";
 import PeaceAdvantage from "../components/PeaceAdvantage";
 
 const NavBar = () => {
-	const menuData = [
-		{
-			path: "/",
-			name: "Flights",
-		},
-		{
-			path: "/covid",
-			name: "Covid-19 Info",
-		},
-		{
-			path: "/about",
-			name: "About",
-		},
-	];
-	const [open, setOpen] = useState(true);
+  const menuData = [
+    {
+      path: "/",
+      name: "Flights",
+    },
+    {
+      path: "/covid",
+      name: "Covid-19 Info",
+    },
+    {
+      path: "/about",
+      name: "About",
+    },
+  ];
+  const [open, setOpen] = useState(true);
 
-	return (
-		<>
-			<header>
-				<nav>
-					<div>
-						<div className="md:flex md:px-10 justify-between items-center">
-							<div className="flex justify-between md:items-center p-4">
-								<div className="ml-10 md:items-center cursor-pointer pt-2 md:py-0">
-									<Link
-										to="/"
-										onClick={() => {
-											!open ? setOpen(!open) : open;
-										}}
-									>
-										{/* logo */}
-										<img
-											src="/5.jpg"
-											alt="flyASAP logo"
-											className="d-inline-block align-top w-16"
-										/>
-										<span className="text-green-900 text-4xl font-bold">
-											fly
-										</span>
-										<span className="text-blue-900 text-4xl font-bold">
-											ASAP
-										</span>
-									</Link>
-								</div>
+  return (
+    <>
+      <header>
+        <nav>
+          <div>
+            <div className="md:flex md:px-10 justify-between items-center">
+              <div className="flex justify-between md:items-center p-4">
+                <div className="ml-10 md:items-center cursor-pointer pt-2 md:py-0">
+                  <Link
+                    to="/"
+                    onClick={() => {
+                      !open ? setOpen(!open) : open;
+                    }}
+                  >
+                    {/* logo */}
+                    <img
+                      src="/5.jpg"
+                      alt="flyASAP logo"
+                      className="d-inline-block align-top w-16"
+                    />
+                    <span className="text-green-900 text-4xl font-bold">
+                      fly
+                    </span>
+                    <span className="text-blue-900 text-4xl font-bold">
+                      ASAP
+                    </span>
+                  </Link>
+                </div>
 
-								<div
-									className="md:hidden cursor-pointer"
-									onClick={() => setOpen(!open)}
-								>
-									<ion-icon
-										name={open ? "menu" : "close"}
-										size="large"
-									></ion-icon>
-								</div>
-							</div>
+                <div
+                  className="md:hidden cursor-pointer"
+                  onClick={() => setOpen(!open)}
+                >
+                  <ion-icon
+                    name={open ? "menu" : "close"}
+                    size="large"
+                  ></ion-icon>
+                </div>
+              </div>
 
-							{/* Nav Links */}
-							{/* top-20 opacity-100 absolute md:static*/}
-							<div className="md:flex">
-								<ul
-									className={`md:flex md:items-center md:justify-between pb-12 md:pb-0 bg-white md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-										open ? "hidden" : "top-[-490px] md:opacity-100 opacity-100"
-									}`}
-								>
-									<div className="md:flex">
-										{menuData.map((item) => (
-											<li className="md:py-0 my-7">
-												<Link
-													to={item.path}
-													key={item.id}
-													className="navlink"
-													onClick={() => setOpen(!open)}
-												>
-													{item.name}
-												</Link>
-											</li>
-										))}
-									</div>
+              {/* Nav Links */}
+              {/* top-20 opacity-100 absolute md:static*/}
+              <div className="md:flex">
+                <ul
+                  className={`md:flex md:items-center md:justify-between pb-12 md:pb-0 bg-white md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+                    open ? "hidden" : "top-[-490px] md:opacity-100 opacity-100"
+                  }`}
+                >
+                  <div className="md:flex">
+                    {menuData.map((item) => (
+                      <li key={item.id} className="md:py-0 my-7">
+                        <Link
+                          to={item.path}
+                          // key={item.id}
+                          className="navlink"
+                          onClick={() => setOpen(!open)}
+                        >
+                          {item.name}
+                        </Link>
+                      </li>
+                    ))}
+                  </div>
 
-									<button className="nav-buttons md:ml-10 ml-10" to="/login">
-										<h6 className="text-2xl font-semibold">Get Started</h6>
-									</button>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</nav>
-			</header>
+                  <button className="nav-buttons md:ml-10 ml-10" to="/login">
+                    <h6 className="text-2xl font-semibold">Get Started</h6>
+                  </button>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
 
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
-		</>
-	);
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 };
 export default NavBar;
 {
-	/* <BookFlightForm />
+  /* <BookFlightForm />
 <OurDestinations />
 <PeaceAdvantage />
 <Vision />
--- src/store.jsx
+++ src/store.jsx
@@ -1,25 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const localStorageUser =
-	localStorage.getItem("user") != null
-		? JSON.parse(localStorage.getItem("user"))
-		: [];
+  localStorage.getItem("user") != null
+    ? JSON.parse(localStorage.getItem("user"))
+    : [];
 
 const initialState = {
-	value: localStorageUser,
+  value: localStorageUser,
 };
 export const userSlice = createSlice({
-	name: "user",
-	initialState,
-	reducers: {
-		addUser: (state, action) => {
-			// state = action.payload
-			state.value.push(action.payload);
-			localStorage.setItem(
-				"user",
-				JSON.stringify(state.value.map((user) => user))
-			);
-		},
-	},
+  name: "user",
+  initialState,
+  reducers: {
+    addUser: (state, action) => {
+      // state = action.payload
+      state.value.push(action.payload);
+      localStorage.setItem(
+        "user",
+        JSON.stringify(state.value.map((user) => user))
+      );
+    },
+  },
 });
 export const { addUser } = userSlice.actions;
