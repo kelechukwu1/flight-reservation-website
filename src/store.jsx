4cf150d056969dd293ce79712e28e41e816ef64b
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-06-30 14:14:37 +0100

    made changes to redux store

--- src/store.jsx
+++ src/store.jsx
@@ -1,25 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const localStorageUser =
-  localStorage.getItem("user") != null
-    ? JSON.parse(localStorage.getItem("user"))
-    : [];
+const localStorageUser = localStorage.getItem("user");
+const parsedLocalStorageUser = localStorageUser
+  ? JSON.parse(localStorageUser)
+  : [];
 
 const initialState = {
-  // value: localStorageUser,
-  value: [],
+  value: parsedLocalStorageUser,
 };
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
     addUser: (state, action) => {
-      // state = action.payload
       state.value.push(action.payload);
-      localStorage.setItem(
-        "user",
-        JSON.stringify(state.value.map((user) => user))
-      );
+      // localStorage.setItem(
+      //   "user",
+      //   JSON.stringify(state.value.map((user) => user))
+      // );
+      localStorage.setItem("user", JSON.stringify(state.value));
     },
   },
 });
