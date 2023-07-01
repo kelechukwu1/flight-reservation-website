b96351dc58cc10db00d8c2d62787ace7ea34b514
Kelechukwu <paparazzip8@gmail.com>
2023-07-01 05:50:38 +0100

    .

--- src/pages/BookedFlights.jsx
+++ src/pages/BookedFlights.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const BookedFlights = () => {
-	return <div className="text-4xl">kelechukwu has booked</div>;
+	return (
+		<div className="mx-5">
+			<div className="text-4xl card rounded p-10">kelechukwu has booked</div>
+		</div>
+	);
 };
 
 export default BookedFlights;
